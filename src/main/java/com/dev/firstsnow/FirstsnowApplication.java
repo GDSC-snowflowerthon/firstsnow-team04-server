87dc86f6aea107e885c6eeb15f326de2cdc84ead
gsandoo <rogersddt@naver.com>
2024-01-11 01:54:18 +0900

    Init: initial commit

---
+++ src/main/java/com/dev/firstsnow/FirstsnowApplication.java
@@ -0,0 +1,13 @@
+package com.dev.firstsnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstsnowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstsnowApplication.class, args);
+    }
+
+}
